a3e2efa1ebea4bf6dba921329cd05bdf77c5c6ea
dbqls200 <dbqls200@gmail.com>
2023-01-12 14:41:50 +0900

    Create New Project

---
+++ BookMark/BookMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMarkApp.swift
+//  BookMark
+//
+//  Created by 김유빈 on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct BookMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
