28e63b1aca40ee0185bbc448189acd37c0eab297
dbqls200 <dbqls200@gmail.com>
2023-01-13 18:32:40 +0900

    App Icon Assets 추가

---
+++ BookMark/MyBookList/Views/MyBookListView.swift
@@ -0,0 +1,20 @@
+//
+//  MyBookListView.swift
+//  BookMark
+//
+//  Created by 김유빈 on 2023/01/13.
+//
+
+import SwiftUI
+
+struct MyBookListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyBookListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyBookListView()
+    }
+}
