e505b617bdfa8d9270377c812b6f7f280e0724e4
dbqls200 <dbqls200@gmail.com>
2023-01-14 16:36:12 +0900

    chore: 가독성을 위해 URL을 Image로 변환하는 부분 분리

--- BookMark/BookSearch/Views/Rows/BookSearchRowView.swift
+++ BookMark/BookSearch/Views/Rows/BookSearchRowView.swift
@@ -9,51 +9,33 @@ import SwiftUI
 import URLImage
 
 struct BookSearchRowView: View {
-    var title: String = ""
-    var author: String = ""
-    var publisher: String = ""
-    var imageURL: String = ""
+    var book: Book
         
     var body: some View {
         ZStack {
-            Image("backgroundRoundedRect")
+            Image("backYellowRoundedRect")
                 .resizable()
                 .padding(.horizontal, 12)
                 .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: 130)
             
             HStack(spacing: 12) {
-                URLImage(url: URL(string: imageURL)!,
-                         inProgress: { progress in
-                            ProgressView()
-                                .progressViewStyle(.circular)
-                                .frame(width: 75, height: 105) },
-                         failure: { error,retry in
-                            Rectangle()
-                                .foregroundColor(.lightGray)
-                                .frame(width: 75, height: 105)
-                                .overlay(content: {
-                                    Text("sorry".localized())
-                                        .foregroundColor(.white)
-                                        .font(.system(size: 12, weight: .semibold))
-                                }) },
-                         content: { image in
-                            image
-                                .resizable()
-                                .frame(width: 75, height: 105)
-                        })
-                
+                createImage(url: book.image)
+                                
                 VStack(alignment: .leading, spacing: 8) {
-                    Text(title)
+                    Text(book.title)
                         .lineLimit(2)
+                        .multilineTextAlignment(.leading)
                         .font(.system(size: 16, weight: .bold))
-                    Text(author)
+                    
+                    Text(book.author)
                         .lineLimit(1)
                         .font(.system(size: 14, weight: .semibold))
-                    Text(publisher)
+                    
+                    Text(book.publisher)
                         .lineLimit(1)
                         .font(.system(size: 12, weight: .thin))
                 }
-                .padding(.vertical, 12)
+                .foregroundColor(.black)
                 
                 Spacer()
             }
@@ -63,8 +45,33 @@ struct BookSearchRowView: View {
     }
 }
 
+func createImage(url: String) -> some View {
+    URLImage(url: URL(string: url)!,
+             inProgress: { progress in
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .frame(width: 75, height: 105) },
+             failure: { error,retry in
+                Rectangle()
+                    .foregroundColor(.lightGray)
+                    .frame(width: 75, height: 105)
+                    .overlay(content: {
+                        Text("sorry".localized())
+                            .foregroundColor(.white)
+                            .font(.system(size: 12, weight: .semibold))
+                    }) },
+             content: { image in
+                image
+                    .resizable()
+                    .frame(width: 75, height: 105)
+            })
+
+}
+
 struct BookListRowView_Previews: PreviewProvider {
     static var previews: some View {
-        BookSearchRowView(title: "역행자 TEST", author: "자청 TEST", publisher: "웅진어쩌고 퍼블리셔 TEST", imageURL: "https://")
+        let book: Book = Book(title: "역행자", image: "https://shopping-phinf.pstatic.net/main_3255028/32550285396.20221004114149.jpg", author: "자청 TEST", publisher: "웅진지식하우스 TEST")
+        
+        BookSearchRowView(book: book)
     }
 }
